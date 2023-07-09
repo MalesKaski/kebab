e209347cf342ac154259233af186fca49f1a40c4
Kamil Maleszko <kamil.maleszko@gmail.com>
2023-07-09 23:22:26 +0200

    Update

---
+++ src/main/java/pl/kmaleszko/kebab/KebabApplication.java
@@ -0,0 +1,13 @@
+package pl.kmaleszko.kebab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KebabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KebabApplication.class, args);
+	}
+
+}
